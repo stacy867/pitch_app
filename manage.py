7468fb3e15e6cf4a16cacd59d4bd7c448d04eabc
stacy867 <stacymurenzi@gmail.com>
2019-09-21 17:16:48 +0200

    changes

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 from app import create_app,db
-from app.models import User,Category
+from app.models import User,Category,Pitch,Comment,Vote
 from flask_script import Manager,Server
 from  flask_migrate import Migrate, MigrateCommand
 
@@ -23,6 +23,6 @@ manager.add_command('db',MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User,Category = Category)
+    return dict(app = app,db = db,User = User,Category = Category,Pitch = Pitch,Vote = Vote,Comment = Comment)
 if __name__ == '__main__':
     manager.run()

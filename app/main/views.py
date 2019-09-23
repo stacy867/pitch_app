18044d71e59d1b2d4a0797e75ef8d06313993e0a
stacy867 <stacymurenzi@gmail.com>
2019-09-23 10:00:41 +0200

    added a new function

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
 from .. import db,photos
-from .forms import UpdateProfile,PitchForm
+from .forms import UpdateProfile,PitchForm,CommentForm
 from ..models import  User,Category,Pitch,Comment
 from flask_login import login_required, current_user
 from flask_login import UserMixin
@@ -62,25 +62,36 @@ def update_pic(uname):
     return redirect(url_for('main.profile',uname=uname))
 
 #LIKES AND DISLIKES
-@main.route('/pitch/<int:id>',methods = ['GET','POST'])
-def pitch(id):
-    pitch =Pitch.get_pitches(id)
+@main.route('/downvote/<int:id>',methods = ['GET','POST'])
+def downvotes(id):
+    # pitch =Pitch.get_pitches(id)
+    pitch = Pitch.query.filter_by(id=id).first()
+        
+   
     
-    if request.args.get("upvotes"):
-        pitch.upvotes = pitch.upvotes +1
+    pitch.downvotes = pitch.downvotes + 1
         
-        db.session.add(pitch)
-        db.session.commit()
+    db.session.add(pitch)
+    db.session.commit()
         
-        return redirect("/pitch/{pitch_id}".format(pitch_id=pitch.id))
-    elif request.args.get("downvotes"):
-        pitch.downvotes = pitch.downvotes + 1
+    return redirect("/".format(id=pitch.id))
+@main.route('/upvote/<int:id>',methods = ['GET','POST'])
+def upvotes(id):
+    # pitch =Pitch.get_pitches(id)
+    pitch = Pitch.query.filter_by(id=id).first()
         
-        db.session.add(pitch)
-        db.session.commit()
+    
+    print(pitch)
+    
+   
+    pitch.upvotes = pitch.upvotes +1
         
-        return redirect("/pitch/{pitch_id}".format(pitch_id=pitch.id))
+    db.session.add(pitch)
+    db.session.commit()
         
+    return redirect("/".format(id=pitch.id))
+    
+               
 
 @main.route('/')
 def index():
@@ -108,11 +119,34 @@ def new_pitch(id):
         
     if form.validate_on_submit():
         content = form.content.data
-        new_pitch= Pitch(content=content,category=category.id,user_id=current_user.id)
+        new_pitch= Pitch(content=content,category=category.id,user_id=current_user.id,upvotes=0,downvotes=0)
         new_pitch.save_pitch()
         return redirect(url_for('.index',id=category.id))
     return render_template('new_pitch.html', title = title, pitch_form = form, category = category)
-            
+
+@main.route('/new_comment/<int:id>', methods=['GET','POST'])
+@login_required
+def new_comment(id):
+    '''
+    function that adds comment
+    '''
+    form = CommentForm()
+    comment = Comment.query.filter_by(pitch_id=id).all()
+    pitches = Pitch.query.filter_by(id=id).all()
+    title=f'welcome to pitches'
+    
+    # if category is None:
+    #     abort(404)
+        
+    if form.validate_on_submit():
+        feedback = form.comment.data
+        new_comment= Comment(feedback=feedback,user_id=current_user.id)
+         
+        new_comment.save_comment()
+        return redirect(url_for('.index',))
+    return render_template('comment.html', title = title, comment_form = form,pitches=pitches)
+
+                
 #viewing a Pitch with its comments
 @main.route('/categories/view_pitch/<int:id>', methods=['GET', 'POST'])
 @login_required
@@ -127,6 +161,6 @@ def viewing_pitch(id):
     # if pitches is None:
     #     abort(404)
     comment =Comments.get_comments(id)
-    return render_template('pitch.html', pitches=pitches,category_id=id)
+    return render_template('pitch.html',comment=comment, pitches=pitches,category_id=id)
     
 
